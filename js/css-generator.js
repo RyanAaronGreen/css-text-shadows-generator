4df1b5ef09c1ddb1e84b0015aee73e304c462482
Ryan Green <ryan.green@ziplineinteractive.com>
2017-08-12 15:14:44 -0700

    + Converted Javascript to MVC design pattern to allow for cleaner future updates

--- js/css-generator.js
+++ js/css-generator.js
@@ -1,242 +1,174 @@
 
-// Text Shadow Default Attributes
-var horz = 2;
-var vert = 2;
-var blur = 2;
-
-var red = 0;
-var green = 0;
-var blue = 0;
-var alpha = 0.6;
-
-
-
-// Updates the Textarea with the correct CSS code
-function updateText(horz, vert, blur, red, green, blue, alpha) {
-    document.getElementById('demo-text').style.textShadow = horz + "px " + vert + "px " + blur+ "px rgba(" + red + ", " + green + ", " + blue + ", " + alpha + ")";
-
-    // Check if variable is 0, cause if it is, we don't really need the 'px'
-    horz = (horz == 0 ? horz + " " : horz + "px ");
-    vert = (vert == 0 ? vert + " " : vert + "px ");
-    blur = (blur == 0 ? blur + " " : blur + "px ");
-
-    document.getElementById('your-code').value = 'text-shadow:' + horz + vert + blur + "rgba(" + red + ", " + green + ", " + blue + ", " + alpha + ");";
-}
-
-
-
-// Converts any RGB values into Hex values
-function rgbToHex(r, g, b) {
-    document.getElementById('input--hex').value =  "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-
-
-
-// Updates the initial colors if user enters Hexcode into input
-function updateColors(r, g, b) {
-    document.getElementById('slider--red').value = r;
-    document.getElementById('slider--green').value = g;
-    document.getElementById('slider--blue').value = b;
-    red = r;
-    green = g;
-    blue = b;
-    updateText( horz, vert, blur, r, g, b, alpha);
-}
-
-
-
-// Converts the Hexcode into usable RGB code
-function hexToRgb(hex) {
-    // Example from http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-
-    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-        return r + r + g + g + b + b;
-    });
-
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-
-    var number = result ? {r: parseInt(result[1], 16), g: parseInt(result[2], 16), b: parseInt(result[3], 16)} : null;
-
-    if (number != null) {
-        updateColors(number['r'],  number['g'],  number['b']);
-    }
-}
-
-
-// Slider logic & functionality
-function outputUpdate(element, slider) {
-    var output =  element.getAttribute('data-output');
-    document.querySelector('#' + output).value = slider;
-
-    switch (output) {
-        case 'horz':
-            horz = slider;
-            break;
-        case 'vert':
-            vert = slider;
-            break ;
-        case 'blur':
-            blur = slider;
-            break;
-        case 'red':
-            red = slider;
-            break;
-        case 'green':
-            green = slider;
-            break;
-        case 'blue':
-            blue = slider;
-            break;
-        default:
-            alpha = slider;
-    }
-
-    updateText(horz, vert, blur, red, green, blue, alpha);
-    red = parseInt(red);
-    blue = parseInt(blue);
-    green = parseInt(green);
-
-
-    rgbToHex(red, green, blue);
-}
-
-
-
-
 // Model
-function taskModel(){
+var model = {
+	horz: 2,
+	vert: 2,
+	blur: 2,
+	red: 0,
+	green: 0,
+	blue: 0,
+	alpha: 0.6,
+	hexValue: '#000000',
+	codeValue: 'text-shadow:2px 2px 2px rgba(0, 0, 0, 0.6)'
+};
 
-	var task="";
-	var removeFlag=false;
-
-	this.setTask = function(taskName){
-		task = taskName;
-	}
-
-	this.getTask = function(){
-		return task;
-	}
-
-	this.setFlag = function(flag){
-		removeFlag = flag;
-	}
-
-	this.getFlag = function(){
-		return removeFlag;
-	}
-
-}
-
-// Collection
-function taskCollection(){
-
-	var tasks = [];
-
-	this.getTasks = function(){
-		return tasks;
-	}
-
-	this.setTasks = function(tempTasks){
-		tasks = tempTasks;
-	}
-
-	this.addTask = function(task){
-		tasks.push(task);
-	}
-
-	this.keepTask = function(taskVal,flag){
-		for(index in tasks){
-			if(taskVal==tasks[index].getTask()){
-				tasks[index].setFlag(flag);
-				break;
-			}
-		}
-	}
-}
 
 // View
-function taskView(){
+var view = {
+
+	init: function() {
+		// Hexcode Input
+		this.hex = document.getElementById('input--hex');
+
+		// Placeholder Text
+		this.demoText = document.getElementById('demo-text');
+
+		// Code Placeholder
+		this.code = document.getElementById('your-code');
+
+		// Directional Sliders
+		this.sliderHorz = document.getElementById('slider--horz');
+		this.sliderVert = document.getElementById('slider--vert');
+		this.sliderBlur = document.getElementById('slider--blur');
+
+		// Directional Outputs
+		this.horzOutput = document.getElementById('horz');
+		this.vertOutput = document.getElementById('vert');
+		this.blurOutput = document.getElementById('blur');
+
+		// Color Sliders
+		this.sliderRed = document.getElementById('slider--red');
+		this.sliderGreen = document.getElementById('slider--green');
+		this.sliderBlue = document.getElementById('slider--blue');
+		this.sliderAlpha = document.getElementById('slider--alpha');
+
+		// Color Outputs
+		this.redOutput = document.getElementById('red');
+		this.greenOutput = document.getElementById('green');
+		this.blueOutput = document.getElementById('blue');
+		this.alphaOutput = document.getElementById('alpha');
 
 
-	var template = $('.task','#template');
-	this.showAllTasks=function(taskAllCollection){
+		// Event Listeners
+		this.hex.addEventListener('keyup', function(value){
+			controller.hexToSlider(this.value);
+		});
+		this.sliderHorz.addEventListener('input', function(value){
+			controller.updateHorz(this.value);
+		});
+		this.sliderVert.addEventListener('input', function(value){
+			controller.updateVert(this.value);
+		});
+		this.sliderBlur.addEventListener('input', function(value){
+			controller.updateBlur(this.value);
+		});
+		this.sliderRed.addEventListener('input', function(value){
+			controller.updateRed(this.value);
+		});
+		this.sliderGreen.addEventListener('input', function(value){
+			controller.updateGreen(this.value);
+		});
+		this.sliderBlue.addEventListener('input', function(value){
+			controller.updateBlue(this.value);
+		});
+		this.sliderAlpha.addEventListener('input', function(value){
+			controller.updateAlpha(this.value);
+		});
 
-		var tempCollectionTasks=[];
-		var taskCollection = taskAllCollection.getTasks();
-		var len = taskCollection.length;
+		this.render();
+	},
 
-		$('#task-box').html("");
 
-		for(index=0;index<len;index++){
-			var task = taskCollection[index];
-			var template = $("#template");
-			if(!task.getFlag()){
-				template.find('.task').html(task.getTask());
-				$('#task-box').append(template.html());
-				tempCollectionTasks.push(task);
-			}
-		}
-		taskAllCollection.setTasks(tempCollectionTasks);
+	render: function() {
+		this.sliderRed.value = model.red;
+		this.sliderGreen.value = model.green;
+		this.sliderBlue.value = model.blue;
+		this.sliderAlpha.value = model.alpha;
+
+		this.redOutput.innerHTML = model.red;
+		this.greenOutput.innerHTML = model.green;
+		this.blueOutput.innerHTML = model.blue;
+		this.alphaOutput.innerHTML = model.alpha;
+
+		this.hex.value = model.hexValue;
+		this.demoText.style = model.codeValue;
+		this.code.value = model.codeValue;
 	}
+};
 
-	this.showTask = function(taskModel){
-		var template = $("#template");
-		template.find('.task').html(taskModel.getTask());
-		$('#task-box').append(template.html());
-	}
-
-}
 
 // Controller
-function taskController(collection,view){
+var controller = {
 
-	this.init= function(){
-		$('#add-btn').off('click').on('click',function(){
-			var taskVal = $('#task-name').val();
-			if(taskVal.trim()!=''){
-				$('#task-name').val('');
-				var model = new taskModel();
-				model.setTask(taskVal);
-				collection.addTask(model);
-				view.showTask(model);
-			}
-			else{
-				alert('Please enter some value!!');
-				return;
-			}
+	init: function() {
+		view.init();
+	},
 
+	updateHorz: function(value){
+		model.horz = value;
+		controller.updateText();
+	},
+	updateVert: function(value){
+		model.vert = value;
+		controller.updateText();
+	},
+	updateBlur: function(value){
+		model.blur = value;
+		controller.updateText();
+	},
+	updateRed: function(value){
+		model.red = value;
+		controller.rgbToHex();
+		controller.updateText();
+	},
+	updateGreen: function(value){
+		model.green = value;
+		controller.rgbToHex();
+		controller.updateText();
+	},
+	updateBlue: function(value){
+		model.blue = value;
+		controller.rgbToHex();
+		controller.updateText();
+	},
+	updateAlpha: function(value){
+		model.alpha = value;
+		controller.updateText();
+	},
+	// Updates the Textarea with the correct CSS code
+	updateText: function() {
+		model.codeValue = "text-shadow:" + model.horz + "px " + model.vert + "px " + model.blur + "px " + "rgba(" + model.red + ", " + model.green + ", " + model.blue + ", " + model.alpha + ")";
+		view.render();
+	},
+
+	// Converts any RGB values into Hex values
+	rgbToHex: function() {
+		model.hexValue =  "#" + ((1 << 24) + (parseInt(model.red) << 16) + (parseInt(model.green) << 8) + parseInt(model.blue)).toString(16).slice(1);
+	},
+
+	hexToSlider: function(value){
+		model.hexValue = value;
+
+		// Example from http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+		// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+		var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+		value = value.replace(shorthandRegex, function(m, r, g, b) {
+			return r + r + g + g + b + b;
 		});
-		this.addEvent();
+
+		var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(value);
+
+		var number = result ? {r: parseInt(result[1], 16), g: parseInt(result[2], 16), b: parseInt(result[3], 16)} : null;
+
+		if (number != null) {
+			model.red = number['r'];
+			model.green = number['g'];
+			model.blue = number['b'];
+			controller.updateText();
+		}
 	}
+};
 
-	this.addEvent = function(){
-		$('#task-box').off('click').on('click','.close',function(ev){
-			if($(ev.currentTarget).is(':checked')) {
-				$(ev.currentTarget).parent().css('text-decoration','line-through');
-				var taskVal = $(ev.currentTarget).parent().find('.task').text();
-				collection.keepTask(taskVal,true);
-
-			}
-			else{
-				$(ev.currentTarget).parent().css('text-decoration','none');
-				var taskVal = $(ev.currentTarget).parent().find('.task').text();
-				collection.keepTask(taskVal,false)
-			}
-		});
-
-		$('#remove-btn').off('click').on('click',function(){
-			view.showAllTasks(collection);
-		});
-
-	}
-}
-
-var collection = new taskCollection();
-
-var view = new taskView();
-var controller = new taskController(collection,view);
 controller.init();
 
 
